8cfa5926053e6e106fcc9f4024f2726bd4dd4012
Abhishek kumar yadav <abhimgs8511@gmail.com>
2024-11-04 03:38:51 +0530

    Updates

---
+++ precache-manifest.a31273e2ace75e7ea9d773c8f77a3d7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5a1cc1ef50512ed55f03d2ef18fceb",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "f917b98e68a7d22d80d8",
+    "url": "/my-react-app/static/css/main.14da97b3.chunk.css"
+  },
+  {
+    "revision": "66c14fc3229fb6f9ed24",
+    "url": "/my-react-app/static/js/2.53dab314.chunk.js"
+  },
+  {
+    "revision": "09dfcff8991931712817539b48cc9b6c",
+    "url": "/my-react-app/static/js/2.53dab314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b913e9368c01298ec1",
+    "url": "/my-react-app/static/js/3.6f682f8c.chunk.js"
+  },
+  {
+    "revision": "f917b98e68a7d22d80d8",
+    "url": "/my-react-app/static/js/main.156ad803.chunk.js"
+  },
+  {
+    "revision": "b9c92a3b1c35688484cf",
+    "url": "/my-react-app/static/js/runtime-main.20993491.js"
+  }
+]);
